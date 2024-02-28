b3fcb3578bb78fd490a9a7c242fac79e9e545113
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-02-28 16:14:42 +0900

    次はsupabaseから 動画 ~19:31

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
-import Link from "next/link";
+import BBSCardList from "./components/BBSCardList";
 
 export default function Home() {
   return (
-    <main className=" grid lg:grid-cols-3">
-      <Card className="" >
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum doloremque voluptas, impedit voluptates a ab aperiam alias autem libero. Placeat, delectus officiis repellat animi excepturi illum impedit voluptatibus quaerat laboriosam?
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Link href={"/bbs-posts/1"}>Read More</Link>
-      </CardFooter>
-    </Card>
+    <main >
+      <BBSCardList />
     </main>
   );
 }
