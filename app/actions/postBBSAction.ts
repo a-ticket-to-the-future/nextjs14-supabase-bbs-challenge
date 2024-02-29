787ed25c8afda2fe3fef02c795553ab94e108929
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-02-29 16:14:18 +0900

    オリジナル注釈追加

--- app/actions/postBBSAction.ts
+++ app/actions/postBBSAction.ts
@@ -7,7 +7,7 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export const postBBS = async ({username,title,content}:z.infer<typeof formSchema>) => {
-
+    //z.infer<typeof formSchema>はzodが用意している部分型抜き用の記法
     // const { username, title, content} = await  req.json();
 
      await prisma.post.create({
